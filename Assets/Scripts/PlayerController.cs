66034aa2cb8a9be3520cc9a0b1ee8395590a96c6
Leonardo Morselli <leonardo.morselli02@gmail.com>
2026-06-19 22:26:22 +0200

    feat(character): completed character controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 
     
     private Rigidbody2D _rb;
+    [Tooltip("What is considered ground for grounded checks")]
     [SerializeField]
     private LayerMask _floorMask;
 
@@ -14,44 +15,114 @@ public class PlayerController : MonoBehaviour
     private InputActionAsset _playerInput;
     private InputAction _moveAction;
     private InputAction _jumpAction;
+
+    private InputAction _dashAction;
+
     #endregion
 
     [Header("Rider")]
+    [Tooltip("The height of the character")]
     [SerializeField]
     float _rideHeight;
     [SerializeField]
-    float _rideSpringForceStrength, _rideSpringDamper;
+    [Tooltip("The strength of the spring that keeps the character at the specified height")]
+    float _rideSpringForceStrength;
+    [SerializeField]
+    [Tooltip("The damper of the spring strength")]
+    float _rideSpringDamper;
+    [Tooltip("Length of the ray to check the ride height")]
     [SerializeField]
     float _rayLength = 0.5f;
 
     [Header("Movement")]
+    [Tooltip("Speed of the character")]
     [SerializeField]
     private float _speed;
+    [Tooltip("Acceleration of the character to get to max speed")]
     [SerializeField]
     private float _acceleration;
     
     private Vector2 _currentMovement;
 
+    [Tooltip("Max acceleration each frame to get to the desired speed")]
     [SerializeField]
     private float _maxAcceleration;
-
+    [Tooltip("Curve for change of direction strength (Do not change unless you know what you're doing)")]
     [SerializeField]
     private AnimationCurve _accelerationFactorFromDot;
     
     float _move;
 
     [Header("Jump")]
+    [Tooltip("The force of the jump")]
     [SerializeField]
     private float _jumpForce;
+    [Tooltip("Custom gravity")]
+    [SerializeField]
+    private float _gravity;
+    [Tooltip("How hard the force that brings down the player when stops pressing the jump button")]
     [SerializeField]
     private float _downwardForce;
+    [Tooltip("How much time for the player to jump after leaving and hedge")]
     [SerializeField]
     private float _coyoteTime;
+    private float _mayJump;
+    [Tooltip("How much time for the player to buffer a jump before landing")]
+    [SerializeField]
+    private float _inputBufferTime;
+    private float _jumpRequested;
+    [Tooltip("How many jumps (also considering the first jump from the ground)")]
+    [SerializeField]
+    private float _totalJumps;
+    private float _numberOfJumps;
+    [Tooltip("Force multiplier for the second jump onward (to oppose the downward force)")]
+    [SerializeField]
+    private float _additionalJumpsMultiplier;
+
+    [Header("Dash")]
+    [Tooltip("The strength of the dash")]
+    [SerializeField]
+    private float _dashForce;
+    [Tooltip("The cooldown of the dash")]
+    [SerializeField]
+    private float _dashCooldown;
+    private float _dashCooldownTimer;
+    [Tooltip("The duration of the dash (for how long the character is unaffected by gravity)")]
+    [SerializeField]
+    private float _dashDuration;
+    private float _dashDurationTimer;
+    [Tooltip("If true, the character reacts to the inputs of the player during the dash")]
+    [SerializeField]
+    private bool _canMoveDuringDash;
+
 
     [Header("Debug")]
     [SerializeField]
     bool _isWalking;
+    [SerializeField]
     bool _isJumping;
+    [SerializeField]
+    bool _hasLanded;
+    [SerializeField]
+    bool _lookingRight;
+    [SerializeField]
+    bool _dashing;
+
+    [SerializeField]
+    bool _isStopped;
+    Vector2 _velocityBeforeStopping;
+
+
+    private bool _wasGrounded;
+    private bool Grounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, _rideHeight, _floorMask);
+        if (hit)
+        {   
+            return true;
+        }            
+        return false;   
+    }
 
 
 
@@ -64,6 +135,12 @@ public class PlayerController : MonoBehaviour
         _jumpAction = _playerInput.FindActionMap("Player").FindAction("Jump");
         _jumpAction.performed += OnJump;
         _jumpAction.canceled += OnJumpRelease;
+        _dashAction = _playerInput.FindActionMap("Player").FindAction("Sprint");
+        _dashAction.performed += OnDash;
+        _numberOfJumps = 0;
+        _jumpRequested = 0;
+        _dashCooldownTimer = 0;
+        _dashDurationTimer = 0;
     }
 
     private void HandleRideHeight()
@@ -103,9 +180,19 @@ public class PlayerController : MonoBehaviour
     private void HandleMovement()
     {
         
-//        Debug.Log("Movement " + _rb.linearVelocity);
-       // _rb.AddForce(movement * _speed * Time.fixedDeltaTime, ForceMode2D.Force);
-       // Debug.Log(_rb.linearVelocity);
+        //add to the dash cooldown
+        _dashCooldownTimer += Time.fixedDeltaTime;
+        //if dashing add to the duration timer
+        if (_dashing)
+        {
+            _dashDurationTimer += Time.fixedDeltaTime;
+            //if we are reaching the dash duration
+            if (_dashDurationTimer > _dashDuration)
+            {
+                //no more dashing
+                _dashing = false;
+            }
+        }
 
         //calculate the new goal velocity
         Vector3 unitVel = _currentMovement.normalized;
@@ -127,20 +214,57 @@ public class PlayerController : MonoBehaviour
          neededAccel = Vector3.ClampMagnitude(neededAccel, maxAccel);
         _rb.AddForce(Vector3.Scale(neededAccel * _rb.mass, new Vector2(1f, 0)));
 
+
+
     }
 
     private void HandleVerticalMovement()
     {   
+        bool grounded = Grounded();
         //if we reached the peak of the jump
         if (_isJumping && _rb.linearVelocityY < 0)
         {
             //start descending
             ApplyDownForce();
+            //_isJumping = false;
+        }
+
+        _hasLanded = !_wasGrounded && grounded;
+        _wasGrounded = grounded;
+        if (_hasLanded && !_isJumping)
+        {
+            _numberOfJumps = 0;
+            _mayJump = 0;
+        }
+
+        if (grounded)
+        {
+            //if we inputted before the timer
+            if (_jumpRequested < _inputBufferTime)
+            {
+                Jump();
+                _jumpRequested = 10f;
+            }
+                
+        } 
+        else
+        {
+            if (!_dashing)
+                _rb.linearVelocityY -= _gravity * Time.fixedDeltaTime;
+            
+            _jumpRequested += Time.fixedDeltaTime;
+            _mayJump += Time.fixedDeltaTime;
         }
     }
 
     void OnMove(InputAction.CallbackContext context)
     {
+        if (_dashing)
+        {
+            _move = 0f;
+           if (!_canMoveDuringDash) return;
+        }
+
         _move = context.ReadValue<Vector2>().x;
         _isWalking = true;
     }
@@ -153,10 +277,39 @@ public class PlayerController : MonoBehaviour
 
     void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jumped");
-        _rb.AddForceY(_jumpForce, ForceMode2D.Impulse);
-        _isJumping = true;
+        if (Grounded() || (_mayJump < _coyoteTime && !_isJumping) || _numberOfJumps < _totalJumps)
+        {
+            Debug.Log("Jumps: " + _numberOfJumps + " Grounded: " + Grounded() + " Coyote: " + (_mayJump < _coyoteTime && !_isJumping) + " jumps: " + (_numberOfJumps < _totalJumps));
+           Jump();
+        } else
+        {
+            _jumpRequested = 0;
+        }
+    }
 
+    void OnDash(InputAction.CallbackContext context)
+    {
+       
+        if (_dashCooldownTimer > _dashCooldown)
+        {               
+            //add force in the direction we're going
+            _rb.AddForceX(_dashForce * _move, ForceMode2D.Impulse);
+            //reset vertical velocity
+            _rb.linearVelocityY = 0;
+            _dashing = true;
+            _dashCooldownTimer = 0;
+            _dashDurationTimer = 0;
+        }
+            
+    }
+
+    void Jump()
+    {
+         
+        _rb.AddForceY(_jumpForce * (_numberOfJumps > 0? _additionalJumpsMultiplier : 1), ForceMode2D.Impulse);
+        _isJumping = true;
+        Debug.Log("Jumps: " + _numberOfJumps);
+        _numberOfJumps++;
     }
 
     void OnJumpRelease(InputAction.CallbackContext context)
@@ -166,7 +319,6 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyDownForce()
     {
-        Debug.Log("Released");
         _rb.AddForceY(-_downwardForce, ForceMode2D.Impulse);
         _isJumping = false;
     }
@@ -178,6 +330,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_isStopped)
+        {
+            _rb.linearVelocity = Vector2.zero;
+            return;
+        }
+            
+
         if (!_isJumping)
             HandleRideHeight();
         HandleMovement();
@@ -203,4 +362,19 @@ public class PlayerController : MonoBehaviour
     }
 
 
+    public void StopCharacter(float time = 0f)
+    {
+        _velocityBeforeStopping = _rb.linearVelocity;
+        _isStopped = true;
+        _rb.linearVelocity = Vector2.zero;
+
+        if (time > 0f)
+            Invoke("StartCharacter", time);
+    }
+
+    public void StartCharacter()
+    {
+        _rb.linearVelocity = _velocityBeforeStopping;
+        _isStopped = false;
+    }
 }
