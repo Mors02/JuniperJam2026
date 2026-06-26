be90ba000c74a15467165b00b0bc98b92bd3ceb2
parker-tos <thatoneguy1c@gmail.com>
2026-06-25 21:53:22 -0400

    wheel

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class ParallaxController : MonoBehaviour
 {
     private Vector2 size;
+    public bool lockX;
+    public bool lockY;
     private Vector2 startPos;
     public GameObject cam;
     public float parallaxEffect;
@@ -19,7 +21,7 @@ public class ParallaxController : MonoBehaviour
         Vector2 temp = (cam.transform.position * (1 - parallaxEffect));
         Vector2 dist = (cam.transform.position * parallaxEffect);
 
-        transform.position = new Vector3(startPos.x + dist.x, startPos.y + dist.y, transform.position.z);
+        transform.position = new Vector3(lockX ? startPos.x : startPos.x + dist.x, lockY ? startPos.y :startPos.y + dist.y, transform.position.z);
 
         //if (temp > startpos + length) startpos += length;
         //else if (temp < startpos - length) startpos -= length;
