0150a8cb62fd9890ea29b0e02f2c91c87144751c
Okwara-Favour <65445852+Okwara-Favour@users.noreply.github.com>
2026-06-26 20:45:32 -0700

    Added - EndingScene

---
+++ Assets/Scripts/EndingSceneMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndingSceneMenu : MonoBehaviour
+{
+    [SerializeField] private Button menuButton;
+
+    private Animator _curtainAnimator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameManager.Instance.CurrentWinCon = null;
+
+        GameObject.FindGameObjectWithTag("Curtains").TryGetComponent(out _curtainAnimator);
+
+        StartCoroutine(WaitForOpening());
+
+        if (menuButton)
+        {
+            menuButton.onClick.AddListener(MainMenu);
+        }
+    }
+
+    void MainMenu()
+    {
+        gameObject.SetActive(false);
+
+        GameManager.Instance.CurrentWinCon = null;
+
+        if (LevelEndManager.instance)
+            LevelEndManager.instance.CloseAndLoadScene("MainMenu");
+        else
+            SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    private IEnumerator WaitForOpening()
+    {
+        yield return new WaitForSecondsRealtime(0.20f);
+        _curtainAnimator.SetTrigger("Open");
+    }
+}
--- Assets/Scripts/UI/Curtains.cs
+++ Assets/Scripts/UI/Curtains.cs
@@ -8,11 +8,11 @@ public class Curtains : MonoBehaviour
     public void ActivateWinConAnimation()
     {
         GameManager.Instance.ActivateWinConAnimation();
-        PauseManager.instance.SetPause("curtains", 1f);
+        if (PauseManager.instance) PauseManager.instance.SetPause("curtains", 1f);
     }
 
     public void StopGame()
     {
-        PauseManager.instance.SetPause("curtains", 0f);
+        if (PauseManager.instance) PauseManager.instance.SetPause("curtains", 0f);
     }
 }
